aadaf8609ab12c3366ccfd394633fc2a691b4a29
zakigaebal <zakigaebal@gmail.com>
2022-11-23 18:15:12 +0900

    swagger

---
+++ src/main/java/com/example/mybatis_psql_board/member/controller/MemberApiController.java
@@ -0,0 +1,20 @@
+package com.example.mybatis_psql_board.member.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name="member", description = "회원 api")
+@RequestMapping("/api/member")
+@RestController
+public class MemberApiController {
+
+
+}
